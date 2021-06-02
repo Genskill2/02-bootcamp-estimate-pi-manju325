5eed3cbe3ba21448d89de9b52e6b633e33c14e75
manju890 <manjunathrc321@gmail.com>
2021-06-02 18:39:12 +0530

    solution edited

--- estimate.py
+++ estimate.py
@@ -1,7 +1,7 @@
-def wallis(n):
- value=0
- t=1
- for t in range(1,n):
-  value=value+(8*t**2)/(4*t**2-1)
-  t=+1
+n=int(input())
+value=0
+t=1
+for t in range(1,n):
+ value=value+(8*t**2)/(4*t**2-1)
+ t=+1
  print(value)
