f0cc996b772bf8ee25a07a2294ffdcb97784acf2
manju325 <manjunathrc321@gmail.com>
2021-07-21 05:13:31 +0530

    updated

--- estimate.py
+++ estimate.py
@@ -1,7 +1,21 @@
+import math
+import unittest
+
 def wallis(n):
  value=1
  t=1
  for t in range(1,n):
   value = value*(4*t**2)/((4*t**2)-1)
   t=+1
- print(2*value)
+ return(2*value)
+
+class TestWallis(unittest.TestCase):
+    def test_low_iters(self):
+        for i in range(0, 5):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
+
+    def test_high_iters(self):
+        for i in range(500, 600):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
